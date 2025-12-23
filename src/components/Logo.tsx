e9ab3156b42bcc57a2134cb0fb8f656c3a748df2
Bernardo De la garza <berniedlgg@gmail.com>
2025-12-23 11:15:13 +0000

    asi?

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 
 const Logo = ({ className }: { className?: string }) => {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-xl font-bold font-headline text-primary", className)}>
-      {/* 
-        This is a temporary test using an online SVG. 
-        If you see this logo, it means the issue is with your local file name or location.
-        Please ensure your logo is named 'logotipo-usos-01.svg' and is in the 'public' folder.
-      */}
-      <img 
-        src="https://www.svgrepo.com/show/530599/adobe.svg" 
-        alt="CONFÍA Logo" 
-        style={{ height: '32px', width: 'auto' }}
+      <Image 
+        src="/Logo/logotipo-usos-01.svg" 
+        alt="CONFÍA Logo"
+        width={32}
+        height={32}
+        className="h-8 w-auto"
       />
       <span className="mt-1">CONFÍA</span>
     </Link>
