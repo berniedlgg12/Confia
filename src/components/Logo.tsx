937517920b813e8b8eb844046cdd6e83b901b78e
Bernardo De la garza <berniedlgg@gmail.com>
2025-12-23 11:16:50 +0000

    mas grande

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,9 +9,9 @@ const Logo = ({ className }: { className?: string }) => {
       <Image 
         src="/Logo/logotipo-usos-01.svg" 
         alt="CONFÍA Logo"
-        width={140}
-        height={40}
-        className="h-10 w-auto"
+        width={180}
+        height={50}
+        className="h-12 w-auto"
       />
     </Link>
   );
